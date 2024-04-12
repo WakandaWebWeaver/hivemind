dcbde87a8f767c4612e569eb02ff72c0d75ce79f
Esvin Joshua <72260184+WakandaWebWeaver@users.noreply.github.com>
2024-04-12 17:23:09 +0530

    Created posts page, added features

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import boto3
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
-import json
-from werkzeug.utils import secure_filename
-
+import datetime
+# profanity checker
+from better_profanity import profanity
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
@@ -30,6 +30,10 @@ db_name = os.getenv('MONGO_DB_NAME')
 db = mongo[db_name]
 
 user_collection = db['users']
+posts_collection = db['posts']
+blacklist_collection = db['blacklist']
+
+
 
 
 # User class for Flask-Login
@@ -57,6 +61,9 @@ def login():
         password = request.form['password']
 
         user = user_collection.find_one({'username': username, 'password': password})
+        blacklist = blacklist_collection.find_one({'username': username, 'password': password})
+        if blacklist:
+            return 'You have been blacklisted. Contact the admin for further information.'
         if user:
             if user['username'] == username and user['password'] == password:
                 user_obj = User()
@@ -95,9 +102,9 @@ def register():
                 s3.upload_fileobj(profile_picture, s3_bucket_name, file_key)
                 profile_picture_s3_key = f"Profile pictures/{username}"
             else:
-                profile_picture_s3_key = None
+                profile_picture_s3_key = "Profile pictures/avatar_default.jpeg"
         else:
-            profile_picture_s3_key = None
+            profile_picture_s3_key = "Profile pictures/avatar_default.jpeg"
 
         user_data = {
             'full_name': full_name,
@@ -143,6 +150,74 @@ def download():
     file_url = f"https://{s3_bucket_name}.s3.amazonaws.com/materials/{filename}"
     # file_path = file_url.replace('/Users/esvinjoshua/Desktop/Projects/Python Projects/playground/HiveMind', '')
     return redirect(file_url)
+
+# @app.route('/posts')
+# @login_required
+# def posts():
+#     return render_template('posts.html',session=session,
+#                            profile_picture_url= f"https://{s3_bucket_name}.s3.amazonaws.com/{session['profile_picture']}" if session.get('profile_picture') else None
+#                            )
+
+@app.route('/create_post_page')
+@login_required
+def create_post_page():
+    return render_template('create_post.html',session=session,
+                           profile_picture_url= f"https://{s3_bucket_name}.s3.amazonaws.com/{session['profile_picture']}" if session.get('profile_picture') else None
+                           )
+
+@app.route('/create_post', methods=['POST'])
+@login_required
+def create_post():
+    title = request.form.get('post_title')
+    content = request.form.get('post_text')
+    author = session['name']
+    post_id = posts_collection.count_documents({}) + 1
+    date = datetime.datetime.now().strftime("%Y-%m-%D %H:%M")
+    profile_picture = session['profile_picture']
+
+    if profanity.contains_profanity(content) or profanity.contains_profanity(title):
+        blacklist_collection.insert_one({'title': title,
+                                        'content': content,
+                                        'author': author,
+                                        'profile_picture': profile_picture,
+                                        'date': date,
+                                        'reason for blacklist': 'Profanity is not allowed'
+                                    })
+        return 'Profanity is not allowed'
+
+    post_data = {
+        'title': title,
+        'content': content,
+        'author': author,
+        'profile_picture': profile_picture,
+        'date': date,
+        'post_id': post_id
+
+    }
+
+    posts_collection.insert_one(post_data)
+
+    return redirect(url_for('view_posts'))
+
+
+@app.route('/posts')
+@login_required
+def view_posts():
+    posts = posts_collection.find()
+
+    return render_template('posts.html', posts=posts,
+                           session=session,
+                           builder_url=f"https://{s3_bucket_name}.s3.amazonaws.com/",
+                           profile_picture_url= f"https://{s3_bucket_name}.s3.amazonaws.com/{session['profile_picture']}" if session.get('profile_picture') else None
+                           )
+
+@app.route('/delete_post', methods=['POST'])
+@login_required
+def delete_post():
+    print(request.form)
+    post_id = request.form.get('post_id')
+    posts_collection.delete_one({'post_id': post_id})
+    return redirect(url_for('view_posts'))
 
 
 if __name__ == '__main__':
