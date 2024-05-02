043dbb1f445817a417d67d9f124b619a97a80a8e
Esvin Joshua <72260184+WakandaWebWeaver@users.noreply.github.com>
2024-05-02 16:43:47 +0530

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,47 +1,47 @@
-import easyocr
-import requests
-from better_profanity import profanity
+# import easyocr
+# import requests
+# from better_profanity import profanity
 
 
-reader = easyocr.Reader(['en'], detector='dbnet18')
+# reader = easyocr.Reader(['en'], detector='dbnet18')
 
 def scan_web_url_image(url):
-  try:
-    result = reader.readtext(url)
+#   try:
+#     result = reader.readtext(url)
 
-    text_strings = []
-    for (bbox, text, prob) in result:
-        text_strings.append(text)
+#     text_strings = []
+#     for (bbox, text, prob) in result:
+#         text_strings.append(text)
 
-    return text_strings
+#     return text_strings
 
-  except requests.exceptions.RequestException as e:
-    print(f"Error: Could not fetch image from URL: {e}")
-    return []
-
+#   except requests.exceptions.RequestException as e:
+#     print(f"Error: Could not fetch image from URL: {e}")
+#     return []
+    return [""]
 
 def find_matching_text(college_name, name, s3_image_url):
-    try:
-        result = scan_web_url_image(s3_image_url)
-        for text in result:
-            print(text)
-            if college_name.lower() and name.lower() in str(text).lower():
-                print(f"Found matching text: {text}")
-                return True
+    # try:
+    #     result = scan_web_url_image(s3_image_url)
+    #     for text in result:
+    #         print(text)
+    #         if college_name.lower() and name.lower() in str(text).lower():
+    #             print(f"Found matching text: {text}")
+    #             return True
               
-    except Exception as e:
-        print(f"Error: Could not find matching text: {e}")
-        return False
-
+    # except Exception as e:
+    #     print(f"Error: Could not find matching text: {e}")
+    #     return False
+    return False
 
 def check_image_for_profanity(s3_image_url):
-    try:
-        result = scan_web_url_image(s3_image_url)
-        for text in result:
-            if profanity.contains_profanity(text):
-                return True
-            else:
-                return False
-    except Exception as e:
-        print(f"Error: Could not check image for profanity: {e}")
+    # try:
+    #     result = scan_web_url_image(s3_image_url)
+    #     for text in result:
+    #         if profanity.contains_profanity(text):
+    #             return True
+    #         else:
+    #             return False
+    # except Exception as e:
+    #     print(f"Error: Could not check image for profanity: {e}")
         return False
